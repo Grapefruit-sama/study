7cc6aec7d33360d2c56624fe7708eb87e189ed54
Grapefruit-sama <valera.shubin.1999@gmail.cm>
2021-05-24 19:29:33 +0300

    Багфикс (да я тупой)

--- Glava 5 ch2.py
+++ Glava 5 ch2.py
@@ -18,39 +18,39 @@ while choice != '0':
         4 - Сортировать список
         '''
         )
-    choice = input('Выш выбор: ')
+    choice = input('Ваш выбор: ')
     print()
 
-# Выход их программы
-if choice == '0':
-    print('До свидания.')
+    # Выход их программы
+    if choice == '0':
+        print('До свидания.')
 
-# Вывод результатов на экран
-elif choice == "1":
-    print('Рекорды')
-    for score in scores:
-        print(score)
+    # Вывод результатов на экран
+    elif choice == "1":
+        print('Рекорды')
+        for score in scores:
+            print(score)
 
-# Добавление рекорда
-elif choice == '2':
-    score = int(input('Введите свой рекорд: '))
-    scores.append(score)
+    # Добавление рекорда
+    elif choice == '2':
+        score = int(input('Введите свой рекорд: '))
+        scores.append(score)
 
-# Удаление рекорда
-elif choice == '3':
-    score = int(input('Какой из рекордов удалить?: '))
-    if score in scores:
-        scores.remove(score)
+    # Удаление рекорда
+    elif choice == '3':
+        score = int(input('Какой из рекордов удалить?: '))
+        if score in scores:
+            scores.remove(score)
+        else:
+            print(f'Результат {score} не содежится в списке рекордов')
+
+    # Сортировка рекордов
+    elif choice == '4':
+        scores.sort(reverse=True)
+
+    # Непонятный пользовательский ввод
     else:
-        print(f'Результат {score} не содежится в списке рекордов')
-
-# Сортировка рекордов
-elif choice == '4':
-    scores.sort(recerse=True)
-
-# Непонятный пользовательский ввод
-else:
-    print(f'Извините, в меню нет пункта {choice}')
+        print(f'Извините, в меню нет пункта {choice}')
 
 # Конец программы
 input("\nНажмите Enter. чтобы выйти.")
