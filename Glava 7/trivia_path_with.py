e92a434853590d2cf1d76b5848091d9a2d030b8b
Grapefruit-sama <valera.shubin.1999@gmail.cm>
2021-06-16 22:02:42 +0300

    Программа "Викторина" работа с with и path

--- Glava 7/trivia_path_with.py
+++ Glava 7/trivia_path_with.py
@@ -4,6 +4,8 @@
 
 import sys
 
+from pathlib import Path
+
 from typing import (
     NoReturn,
     IO,
@@ -11,17 +13,18 @@ from typing import (
 )
 
 
-def open_file(file_name: str, mode: str) -> IO:
+def open_file(file_path: Path, mode: str) -> IO:
     """ Открывает файл. """
 
-    try:
-        the_file = open(file_name, mode, encoding='utf-8')
-    except IOError as e:
-        print(f'Невозмжно открыть файл {file_name}. Работа программы будет завершена.\n {e}')
+    # Проверка существует ли файл
+    if file_path.is_file():
+        the_file = open(file_path, mode, encoding='utf-8')
+        return the_file
+    # Если файла не существует
+    elif not file_path.is_file():
+        print(f'Невозмжно открыть файл {file_path}. Работа программы будет завершена.\n')
         input('\nНажмите Enter чтобы выйти')
         sys.exit()
-    else:
-        return the_file
 
 
 def next_line(the_file: IO) -> str:
@@ -38,9 +41,13 @@ def next_block(the_file: IO) -> Union[str, list]:
     category = next_line(the_file)
     question = next_line(the_file)
     answers = []
+
+    # получение вариантов ответа из файла
     for i in range(4):
         answers.append(next_line(the_file))
     correct = next_line(the_file)
+
+    # проверка. После того как correct становится пустой строкой main() завершает игру
     if correct:
         correct = correct[0]
     denomination = next_line(the_file)
@@ -53,6 +60,7 @@ def par(denomination: str) -> int:
 
     digit = '1234567890'
     point = ''
+
     for number in denomination:
         if number in digit:
             point += number
@@ -70,54 +78,71 @@ def welcome(title: str) -> NoReturn:
 def write_record(score: int) -> NoReturn:
     """ Записывает рекорд с именем и очками в файл. """
 
-    name = input('Введите имя: ')
-    
+    # Ввод имени
+    name = input('Введите имя: \n')
+
+    # Запись имени и результата викторины в файл
     score = str(score)
     record = name + ' ' + score + '\n'
-    with open("Glava 7\\record_list.txt", 'a+') as record_list:
+    with open(Path("Glava 7", "record_list.txt"), 'a+') as record_list:
         record_list.write(record)
 
 
 def display_record() -> NoReturn:
     """ Отображает список рекордов из файла"""
-    with open("Glava 7\\record_list.txt", 'r') as record_list:
+
+    with open(Path("Glava 7", "record_list.txt"), 'r') as record_list:
         print(record_list.read())
 
 
 def main():
-    trivia_file = open_file("Glava 7\\trivia.txt", 'r')
-    title = next_line(trivia_file)
-    welcome(title)
-    score = 0
-    # Извлечение первого блока
-    category, question, answers, correct, denomination, explanation = next_block(trivia_file)
-    while category:
-        print(category)
-        print(question)
-        print('Вопрос наминалом в :', denomination)
-        for i in range(4):
-            print(f'\t {i + 1} - {answers[i]}')
-        # Получение ответа
-        answer = input("Ваш ответ: ")
-        # проверка ответа
-        if answer == correct:
-            print("\nДа!", end=" ")
-            score += par(denomination)
-        else:
-            print("\nНет не верно", end=" ")
-        print(explanation)
-        print(f'Счет {score} \n\n')
-        # переход к следующему вопросу
+    with open_file(Path("Glava 7", "trivia.txt"), 'r') as trivia_file:
+        title = next_line(trivia_file)
+        welcome(title)
+        score = 0
+
+        # Извлечение первого блока
         category, question, answers, correct, denomination, explanation = next_block(trivia_file)
-    trivia_file.close()
+        while category:
+            # Вывод строк
+            print(category)
+            print(question)
+            print('Вопрос наминалом в :', denomination)
+
+            # Отображение вариантов ответа
+            for i in range(4):
+                print(f'\t {i + 1} - {answers[i]}')
+
+            # Получение ответа
+            answer = input("Ваш ответ: ")
+
+            # проверка ответа
+            if answer == correct:
+                print("\nДа!", end=" ")
+                score += par(denomination)
+            else:
+                print("\nНет не верно", end=" ")
+
+            # Вывод после ответа
+            print(explanation)
+            print(f'Счет {score} \n\n')
+
+            # переход к следующему вопросу
+            category, question, answers, correct, denomination, explanation = next_block(trivia_file)
+
+    # Уведомление об окончании виктарины
     print('Это был последний вопрос!')
     print('На вашем счету', score)
+
+    # Запись рекордов
     y_or_n = input('Хотите ли вы записать рекорд? (y/n): ')
     if y_or_n == 'y':
         write_record(score)
     else:
         pass
-    y_or_n = input('Хотите ли вы посмотреть список рекордов? (y/n): ')
+
+    # Просмотр таблицы рекордов
+    y_or_n = input('Хотите ли вы посмотреть список рекордов? (y/n): \n')
     if y_or_n == 'y':
         display_record()
     else:
